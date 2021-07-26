d865d61cc20cbf7f7851f7d1a5df0d10c44a6008
Anton Kesy <anton@kesy.de>
2021-07-26 20:31:10 +0200

    Update mandelbrot-set-simple.h

--- mandelbrot-set-simple/mandelbrot-set-simple.h
+++ mandelbrot-set-simple/mandelbrot-set-simple.h
@@ -1,4 +1,21 @@
 #pragma once
+//Anton Kesy
+
+
+#include <cmath>
+
+#include "framework.h"
+
+#include <vector>
+
+
+#define MAX_LOADSTRING 100
+
+#define AMOUNT_OF_THREADS 4U
+
+
+#define MAX_ITERATIONS 255
+
 
 struct ComplexNumber
 {
@@ -7,6 +24,7 @@ struct ComplexNumber
 };
 
 typedef struct ComplexNumber sComplexNumber_t;
+typedef sComplexNumber_t* psComplexNumber_t;
 
 struct ComplexArea
 {
@@ -15,6 +33,7 @@ struct ComplexArea
 };
 
 typedef struct ComplexArea sComplexArea_t;
+typedef sComplexArea_t* psComplexArea_t;
 
 
 typedef struct
@@ -28,15 +47,24 @@ typedef struct
 } sDrawMandelbrotThreadData_t;
 
 void DrawMandelbrot(HDC hdc, RECT rect, sComplexArea_t sComplexArea);
-
 sComplexNumber_t DoTransformationPixel2ComplexNumber(unsigned int uiX, unsigned int uiY, sComplexArea_t sComplexArea,
                                                      RECT rect);
-
 unsigned int GetMandelbrotIterations(sComplexNumber_t sComplexNumber);
-
 double AbsComplexNumber(sComplexNumber_t sComplexNumber);
 
 DWORD WINAPI ThreadDrawMandelbrot(void* pParam);
+
+// Global Variables:
+HINSTANCE hInst; // current instance
+WCHAR szTitle[MAX_LOADSTRING]; // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING]; // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM MyRegisterClass(HINSTANCE hInstance);
+BOOL InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
+
 
 void DrawMandelbrotBitMap(HDC hdc, RECT rect);
 
